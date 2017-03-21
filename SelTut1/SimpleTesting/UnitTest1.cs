b3c4473adfe8bf6f034aa4b925e569353479cbae
Michał Dudziak <dudziak.michal@gmail.com>
2017-03-21 19:26:57 +0100

    second project - verify sum of two int Tutorial added

---
+++ SelTut1/SimpleTesting/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int x = 5;
+            int y = 10;
+
+            int sum = x + y;
+
+            Assert.AreEqual(15, sum);
+        }
+    }
+}
