6f463377b0fe44c5f910773bf052bcf320cd8b85
Michał Dudziak <dudziak.michal@gmail.com>
2017-03-21 19:23:03 +0100

    first project

---
+++ SelTut1/SelTut1/FirstSelTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelTut1
+{
+
+    class FirstSelTest
+    {
+        [Test]
+        public void FirstTest()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://pl.wikipedia.org/");
+            string welcome = driver.FindElement(By.Id("main-page-welcome")).Text;
+            Assert.IsTrue(welcome.Contains("Witaj w"));
+            driver.Quit();
+        }
+    }
+}
